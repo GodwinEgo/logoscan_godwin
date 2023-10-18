1619d288571391e548f06d19da341064bd84ec50
GodwinEgo <godwinehile04@gmail.com>
2023-10-18 14:37:00 +0100

    login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,57 +1,82 @@
-import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/navbar.jsx";
 
 const Login = () => {
-  const navigate = useNavigate(); // Moved this outside of handleLoginClick
+  const url = import.meta.env.VITE_BACKEND_API_URL;
+  const formInput = { username: "", password: "" };
+  const navigate = useNavigate();
+  const [user, setUser] = useState(formInput);
+  const [error, setError] = useState(null);
 
-  const handleLoginClick = () => {
-    navigate("/admin_upload"); // Use navigate directly here
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault(); // Prevent the form from submitting
-    handleLoginClick(); // Redirect to "/admin_upload"
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("username", user.username);
+    formData.append("password", user.password);
+    let res = await fetch(`${url}upload-logo/admin/login/`, {
+      method: "POST",
+      body: formData,
+    });
+    let data = await res.json();
+    if (data.authenticated) {
+      navigate("/admin_upload");
+    } else {
+      setUser(formInput);
+      setError("Wrong credentials");
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
   };
 
   return (
-    <div className="container-fluid w-100 m-0 p-0">
+    <>
       <Navbar />
-      <form
-        action=""
-        className="d-flex flex-column justify-content-center align-items-center"
-        style={{ width: "80%", height: "70vh", marginLeft: "10%" }}
-        onSubmit={onSubmit}
+
+      <div
+        style={{ height: "70vh" }}
+        className="container-fluid d-flex flex-column justify-content-center  form-container"
       >
-        <div className="pt-4 col-12">
-          <h4>Login</h4>
-        </div>
-        <div className="py-3 col-12">
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-            className="form-control"
-          />
-        </div>
-        <div className="py-3 col-12">
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="form-control"
-          />
-        </div>
-        <div className="py-3 col-12">
-          <input
-            type="submit"
-            value="Login"
-            className="btn btn-danger form-control"
-          />
-        </div>
-      </form>
-    </div>
+        {error && <p className="mt-3 text-warning">{error}</p>}
+        <form action="" style={{ width: "80%" }}>
+          <div className="pt-4 col-12">
+            <h4 className="text-danger text-center ">Login</h4>
+          </div>
+          <div className="py-3 col-12">
+            <input
+              style={{ width: "100%" }}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              className="form-control"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+            />
+          </div>
+          <div className="py-3 col-12">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="form-control"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+          </div>
+          <div className="py-3 col-12">
+            <input
+              type="submit"
+              value="Login"
+              className="btn form-control btn-danger"
+              onClick={(e) => handleSubmit(e)}
+            />
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
